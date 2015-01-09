cac1339316c45f1fe4ab0b0a910c19ac55539acc
sassy <sassy.watanabe@gmail.com>
2015-01-09 18:26:41 +0900

    create dl-image-twiitter command.

--- README.md
+++ README.md
@@ -3,3 +3,22 @@ DLImageFromTwitter
 
 Download Image From Twitter.
 
+# Usage
+
+* `dl-image-twitter <twitter_id>`
+* Download from twitter id.
+* prepare `key.json` file required to authenticate your account.
+
+# key.json format
+
+```
+{
+    "consumer_key" : "CONSUMER_KEY",
+    "consumer_secret" : "CONSUMEr_SECRET",
+    "access_token_key" : "ACCESS_TOKEN_KEY",
+    "access_token_secret" : "ACCESS_TOKEN_SECRET"
+}
+```
+
+
+
--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 /**
- *  Copyright (c) 2014 Satoshi Watanabe
+ *  Copyright (c) 2014-2015 Satoshi Watanabe
  */
 
 var util = require('util');
 var twitter = require('twitter');
 var fs = require('fs');
-
-if (process.argv.length < 3) {
-    console.log("missing argument.");
-    return;
-}
-
-var twitter_id = process.argv[2];
-
-if (!fs.existsSync("./key.json")) {
-    console.log("you should provide key.json");
-    return;
-}
-var json = fs.readFileSync("./key.json", "utf-8");
-
-var twit = new twitter(JSON.parse(json));
 
 function download(urlStr) {
     var dirname = "image"
@@ -39,15 +24,40 @@ function download(urlStr) {
     });
 }
 
-twit.get('/statuses/user_timeline.json', {screen_name: twitter_id , include_rts : false , count : 200 }, function(datas) {
-    datas.forEach(function(data) {
-        if (data.extended_entities && data.extended_entities.media) {
-            var medias = data.extended_entities.media;
-            medias.forEach(function(media) {
-                if (media.type === "photo") {
-                    download(media.media_url);
-                }
-            });
-        }
+module.exports.exec = function(argv) {
+    if (argv.length < 3) {
+        console.log("missing argument.");
+        return;
+    }
+
+    var twitter_id = argv[2];
+    if (!fs.existsSync("./key.json")) {
+        console.log("you should provide key.json");
+        return;
+    }
+    var json = fs.readFileSync("./key.json", "utf-8");
+
+    var twit = new twitter(JSON.parse(json));
+
+
+    twit.get('/statuses/user_timeline.json', {screen_name: twitter_id , include_rts : false , count : 200 }, function(datas) {
+        datas.forEach(function(data) {
+            if (data.extended_entities && data.extended_entities.media) {
+                var medias = data.extended_entities.media;
+                medias.forEach(function(media) {
+                    if (media.type === "photo") {
+                        download(media.media_url);
+                    }
+                });
+            }
+        });
     });
-});
+}
+
+
+
+
+    
+
+
+
