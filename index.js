fe5db7afb14ed28d20408ebebfb5a5adf0889b44
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-05 18:42:06 +0900

    var -> const

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
  *  Copyright (c) 2014-2020 Satoshi Watanabe
  */
 
-var twitter = require('twitter');
-var fs = require('fs');
-var yargs = require('yargs');
+const twitter = require('twitter');
+const fs = require('fs');
+const yargs = require('yargs');
 
 function download(urlStr, dirname) {
-    var url = require('url');
-    var http = require("http");
-    var filename = url.parse(urlStr).pathname.split('/')[2];
+    const url = require('url');
+    const http = require("http");
+    const filename = url.parse(urlStr).pathname.split('/')[2];
     try {
         fs.statSync(dirname);
     } catch(e) {
@@ -24,28 +24,28 @@ function download(urlStr, dirname) {
 }
 
 module.exports.exec = function() {
-    var argv = yargs.argv;
+    const argv = yargs.argv;
     if (argv._.length < 1) {
         console.log("missing argument.");
         return;
     }
 
-    var twitter_id = argv._[0];
+    const twitter_id = argv._[0];
 
-    var key_path = argv.k ? argv.k : "./key.json";
+    const key_path = argv.k ? argv.k : "./key.json";
     if (!fs.existsSync(key_path)) {
         console.log("you should provide key.json");
         return;
     }
-    var json = fs.readFileSync(key_path, "utf-8");
-    var twit = new twitter(JSON.parse(json));
+    const json = fs.readFileSync(key_path, "utf-8");
+    const twit = new twitter(JSON.parse(json));
 
-    var dirname = argv.d ? argv.d : "image";
+    const dirname = argv.d ? argv.d : "image";
 
     twit.get('/statuses/user_timeline.json', {screen_name: twitter_id , include_rts : false , count : 200 }, function(datas) {
         datas.forEach(function(data) {
             if (data.extended_entities && data.extended_entities.media) {
-                var medias = data.extended_entities.media;
+                const medias = data.extended_entities.media;
                 medias.forEach(function(media) {
                     if (media.type === "photo") {
                         download(media.media_url, dirname);
